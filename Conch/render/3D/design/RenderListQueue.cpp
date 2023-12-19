090fff00ff4e3a8345fe5f1cfb286a21dabda6df
lirongf <619213582@qq.com>
2023-12-19 17:18:05 +0800

    renderlistqueue abstract

---
+++ Conch/render/3D/design/RenderListQueue.cpp
@@ -0,0 +1,39 @@
+#include "RenderListQueue.h"
+namespace layaRender
+{
+    RenderListQueue(cullfun cull, batchfun batch, composefun compose) {
+        this._cull = cull;
+        this._batch = batch;
+        this._compose = compose;
+    }
+
+    RenderListQueue::~RenderListQueue() {
+       _context.reset();
+    }
+
+    uint32_t RenderListQueue::renderQueue(){
+        //cull
+        _cull(_elements, _context->cameraInfo);
+        //batchqueue
+        _batch(_elements);
+        //compose
+        _compose(_elements);
+        //render
+        _context->drawRenderElementList(_elements);
+    }
+
+    void RenderListQueue::addRenderElement(RenderElement* value){
+        //TODO
+        //_elements.add(renderelement);
+    }
+
+    void RenderListQueue::clear() {
+        //_elements.clear();
+    }
+
+    void RenderListQueue::set_context(RenderContext::p_context context) {
+        _context = context;
+    }
+
+
+}
---
+++ Conch/render/3D/design/RenderListQueue.h
@@ -0,0 +1,100 @@
+﻿
+#ifndef RenderListQueue
+#define RenderListQueue
+
+#include <vector>
+#include <functional>
+
+//RenderElement
+
+namespace layaRender
+{
+    class CameraInfo
+    {
+    public:
+        CameraInfo();
+        ~CameraInfo();
+
+    private:
+
+    };
+
+    class RenderContext
+    {
+    public:
+        using p_context = std::shared_ptr<Buffer>;
+        static p_context create() {
+            return std::make_shared<RenderContext>();
+        }
+        ~RenderContext();
+    private:
+        RenderContext();
+    public:
+        CameraInfo* cameraInfo;
+  
+    };
+
+    template <class T>
+    class SingleList
+    {
+    public:
+        SingleList();
+        ~SingleList();
+
+    private:
+
+    };
+
+    class RenderDataElement
+    {
+    public:
+        RenderDataElement();
+        ~RenderDataElement();
+
+    private:
+
+    };
+
+
+    class RenderElement
+    {
+    public:
+        struct geometryData
+        {
+            transform
+
+                boundbox
+                flag
+
+        }composeData;
+
+        RenderDataElement* renderData;
+
+        RenderElement();
+        ~RenderElement();
+
+    private:
+
+    };
+
+    
+	class RenderListQueue {
+        typedef std::function<void(SingleList<RenderElement*> &allElements), CameraInfo* cameraInfo >  cullfun;
+        typedef std::function<void(SingleList<RenderElement*> &allElements)> batchfun;
+        typedef std::function<void(SingleList<RenderElement*> &allElements), uint32_t left, uint32_t right> composefun;
+    public:
+        RenderListQueue(cullfun cull, batchfun batch, composefun compose);
+        ~RenderListQueue();
+        uint32_t renderQueue();
+        void addRenderElement(RenderElement* value);
+        void clear();
+        void set_context(RenderContext::p_context context);
+    private:
+        cullfun _cull;
+        batchfun _batch;
+        composefun _compose;
+        SingleList<RenderElement*> _elements = nullptr;
+        RenderContext::p_context _context = nullptr;
+	};
+}
+#endif //RenderListQueue
