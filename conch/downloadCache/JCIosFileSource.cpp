cc962ec58d7e32aa29474f7e8fddc15dd7eadd81
helloworldlv <775331175@qq.com>
2023-12-27 11:50:03 +0800

    fix build

---
+++ conch/downloadCache/JCIosFileSource.cpp
@@ -0,0 +1,53 @@
+﻿#include "JCIosFileSource.h"
+
+namespace laya
+{
+
+	bool JCIosFileSource::loadFileContent(const char* sFileName, ALLOCMEM pAllocFunc, void* pUserData, int& nBuffSize)
+    {
+		bool bRet = false;
+        std::string sFullName = m_sRootPath +"/"+ sFileName;
+        FILE* fp = fopen(sFullName.c_str(), "rb");
+        if (!fp) return false;
+        fseek(fp, 0, SEEK_END);
+        nBuffSize = (int)ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        unsigned char* pBuff = pAllocFunc(nBuffSize, pUserData);
+        if (!pBuff)
+        {
+            bRet = false;
+        }
+        else
+        {
+            int nReadSize = fread((char*)pBuff, 1, nBuffSize, fp);
+            if (nReadSize == nBuffSize)
+            {
+                LOGI("found the file in the package:%s\n", sFileName);
+                bRet = true;
+            }
+        }
+        fclose(fp);
+        return bRet;
+	}
+    bool JCIosFileSource::loadFileContent(const char* sFileName, char*& pBuffer, int& nBuffSize)
+    {
+        bool bRet = false;
+        std::string sFullName = m_sRootPath + "/"+sFileName;
+        FILE* fp = fopen(sFullName.c_str(), "rb");
+        if (!fp) return false;
+        fseek(fp, 0, SEEK_END);
+        nBuffSize = (unsigned int)ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        pBuffer = new char[nBuffSize+1];
+        int nReadSize = fread((char*)pBuffer, 1, nBuffSize, fp);
+        if (nReadSize == nBuffSize)
+        {
+            LOGI("found the file in the package:%s\n", sFileName);
+            bRet = true;
+        }
+        pBuffer[nBuffSize] = 0;
+        fclose(fp);
+        return bRet;
+    }
+}
+
--- conch/downloadCache/JCIosFileSource.h
+++ conch/downloadCache/JCIosFileSource.h
@@ -1,12 +1,4 @@
-﻿/**
-@file			JCIosFileSource.h
-@brief			
-@author			James
-@version		1.0
-@date			2016_5_11
-*/
-
-#ifndef __JCIosFileSource_H__
+﻿#ifndef __JCIosFileSource_H__
 #define __JCIosFileSource_H__
 
 #include <stdio.h>
@@ -62,60 +54,13 @@ namespace laya
             fclose(fp);
             return nBuffSize;
         }
-		virtual bool loadFileContent(const char* sFileName, ALLOCMEM pAllocFunc, void* pUserData, int& nBuffSize)
-        {
-			bool bRet = false;
-            std::string sFullName = m_sRootPath +"/"+ sFileName;
-            FILE* fp = fopen(sFullName.c_str(), "rb");
-            if (!fp) return false;
-            fseek(fp, 0, SEEK_END);
-            nBuffSize = (int)ftell(fp);
-            fseek(fp, 0, SEEK_SET);
-            unsigned char* pBuff = pAllocFunc(nBuffSize, pUserData);
-            if (!pBuff)
-            {
-                bRet = false;
-            }
-            else
-            {
-                int nReadSize = fread((char*)pBuff, 1, nBuffSize, fp);
-                if (nReadSize == nBuffSize)
-                {
-                    LOGI("found the file in the package:%s\n", sFileName);
-                    bRet = true;
-                }
-            }
-            fclose(fp);
-            return bRet;
-		}
-        virtual bool loadFileContent(const char* sFileName, char*& pBuffer, int& nBuffSize)
-        {
-            bool bRet = false;
-            std::string sFullName = m_sRootPath + "/"+sFileName;
-            FILE* fp = fopen(sFullName.c_str(), "rb");
-            if (!fp) return false;
-            fseek(fp, 0, SEEK_END);
-            nBuffSize = (unsigned int)ftell(fp);
-            fseek(fp, 0, SEEK_SET);
-            pBuffer = new char[nBuffSize+1];
-            int nReadSize = fread((char*)pBuffer, 1, nBuffSize, fp);
-            if (nReadSize == nBuffSize)
-            {
-                LOGI("found the file in the package:%s\n", sFileName);
-                bRet = true;
-            }
-            pBuffer[nBuffSize] = 0;
-            fclose(fp);
-            return bRet;
-        }
+		virtual bool loadFileContent(const char* sFileName, ALLOCMEM pAllocFunc, void* pUserData, int& nBuffSize);
+
+        virtual bool loadFileContent(const char* sFileName, char*& pBuffer, int& nBuffSize);
+
     private:
         std::string             m_sRootPath;
 	};
 }
-//------------------------------------------------------------------------------
-
-
 #endif //__JCIosFileSource_H__
-
-//-----------------------------END FILE--------------------------------
 
