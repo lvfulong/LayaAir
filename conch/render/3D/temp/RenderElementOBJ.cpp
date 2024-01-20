c9d80c549cbb256ff60011a297059697fd5a4719
lirongf <619213582@qq.com>
2024-01-20 18:30:14 +0800

    思路

--- conch/render/3D/temp/RenderElementOBJ.cpp
+++ conch/render/3D/temp/RenderElementOBJ.cpp
@@ -16,4 +16,12 @@ void RenderElementOBJ::_preUpdatePre(IRenderContext3D* context)
 {
 	//TODO
 }
+
+
+compileShader(DefineDatas* compileDefine) {
+	//根据subpass 和pipeline 得到shaderpass
+	//根据shaderpass 和 defineData 宏来得到shader
+	//得到shader的步骤：1、getCacheShader。2、没有get到就创建shader（脚本）
+
+}
 } // namespace laya
