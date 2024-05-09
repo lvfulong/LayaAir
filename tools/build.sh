e9d25b406202d527359b98966c56a79613adf8c5
helloworldlv <775331175@qq.com>
2024-05-09 12:22:05 +0800

    fix ios link error

--- tools/build.sh
+++ tools/build.sh
@@ -205,7 +205,13 @@ function archive_ios {
     local build_type=$1
     cp ios-${build_type}-arm64/conch/libconch.a armv64
     cp ios-${build_type}-x86_64/conch/libconch.a x86_64
+    
+    cp ios-${build_type}-arm64/ghc/libghc.a armv64
+    cp ios-${build_type}-x86_64/ghc/lighc.a x86_64
 
+    cp ios-${build_type}-arm64/rapidxml/librapidxml.a armv64
+    cp ios-${build_type}-x86_64/rapidxml/librapidxml.a x86_64
+    
     cp ios-${build_type}-arm64/modules/binder/libbinder.a armv64
     cp ios-${build_type}-x86_64/modules/binder/libbinder.a x86_64
     
@@ -216,14 +222,14 @@ function archive_ios {
     cp ios-${build_type}-x86_64/iniparser/libiniparser.a x86_64
     
     cd armv64
-    libtool -static *.a -o libconch.a
+    libtool -static *.a -o libconch_static.a
     cd ..
 
     cd x86_64
-    libtool -static *.a -o libconch.a
+    libtool -static *.a -o libconch_static.a
     cd ..
 
-    lipo -create armv64/libconch.a x86_64/libconch.a -output ${publish_dir}/nativetools/template/ios/LayaRuntime-iOS/libs/libconch.a
+    lipo -create armv64/libconch_static.a x86_64/libconch_static.a -output ${publish_dir}/nativetools/template/ios/LayaRuntime-iOS/libs/libconch.a
 
     strip -S -X ${publish_dir}/nativetools/template/ios/LayaRuntime-iOS/libs/libconch.a
     
