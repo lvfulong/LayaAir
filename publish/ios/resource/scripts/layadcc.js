91de875379e070716f250fc317a2155eb336f202
guozhaokui <guozhaokuiwang@gmail.com>
2024-05-17 16:16:49 +0800

    修改layadcc

--- publish/ios/resource/scripts/layadcc.js
+++ publish/ios/resource/scripts/layadcc.js
@@ -3105,16 +3105,16 @@ __webpack_require__.r(__webpack_exports__);
 
 
 //conch
-(async () => {
-    if (window.conch) {
-        let dcc = new _LayaDCCClient__WEBPACK_IMPORTED_MODULE_0__.LayaDCCClient('http://localhost:7788/');
-        //设置这个地址下的资源加载走DCC模式
-        dcc.pathMapToDCC = 'http://localhost:8899/';
-        //通过DCC的根文件初始化dcc客户端
-        let initok = await dcc.init('http://localhost:7788/version.1.0.0.json', null);
-        dcc.injectToNative3();
-    }
-})();
+// (async ()=>{
+// if(window.conch){
+//     let dcc = new LayaDCCClient('http://localhost:7788/' );
+//     //设置这个地址下的资源加载走DCC模式
+//     dcc.pathMapToDCC= 'http://localhost:8899/';
+//     //通过DCC的根文件初始化dcc客户端
+//     let initok = await dcc.init('http://localhost:7788/version.1.0.0.json',null);
+//     dcc.injectToNative3();
+// }
+// })();
 
 })();
 
