0f370ec8def7641ef485b426d3e4c2db3e4ee51d
helloworldlv <775331175@qq.com>
2024-08-01 20:24:39 +0800

    video

---
+++ modules/video/include/video/VideoPlayer.h
@@ -0,0 +1,18 @@
+﻿#ifndef __VideoPlayer_H__
+#define __VideoPlayer_H__
+
+#include <string>
+
+namespace laya
+{
+class VideoPlayer
+{
+
+  public:
+    VideoPlayer();
+    ~VideoPlayer();
+
+  private:
+};
+} // namespace laya
+#endif
---
+++ modules/video/src/VideoPlayer.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <video/VideoPlayer.h>
+
+namespace laya
+{
+VideoPlayer::VideoPlayer()
+{
+}
+
+VideoPlayer::~VideoPlayer()
+{
+}
+} // namespace laya
