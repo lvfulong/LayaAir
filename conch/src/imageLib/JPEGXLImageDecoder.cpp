f5ac0ae21b411576a874641dcae95df32a1d3a07
helloworldlv <775331175@qq.com>
2024-10-11 11:17:38 +0800

    JPEG XL

---
+++ conch/src/imageLib/JPEGXLImageDecoder.cpp
@@ -0,0 +1,10 @@
+#include "JPEGXLImageDecoder.h"
+#include <cmath>
+#include <utils/JCBuffer.h>
+#include <utils/JCFileSystem.h>
+#include <utils/Preprocessor.h>
+#include <utils/thread/JCWorkerThread.h>
+
+namespace laya
+{
+}
---
+++ conch/src/imageLib/JPEGXLImageDecoder.h
@@ -0,0 +1,17 @@
+#ifndef __JPEGXLImageDecoder_H__
+#define __JPEGXLImageDecoder_H__
+
+#include <functional>
+#include <memory>
+#include <jxl/decode_cxx.h>
+#include <jxl/types.h>
+
+namespace laya
+{
+	void test()
+	{
+		auto enc = JxlEncoderMake(/*memory_manager=*/nullptr);
+	}
+}
+
+#endif
