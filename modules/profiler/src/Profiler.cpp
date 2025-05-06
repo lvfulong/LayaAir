fae74a52a5cab1ade52411791354138827ebf73c
helloworldlv <775331175@qq.com>
2025-05-06 12:31:28 +0800

    [优化]Android 文字clearRect 优化

--- modules/profiler/src/Profiler.cpp
+++ modules/profiler/src/Profiler.cpp
@@ -10,7 +10,9 @@
 
 namespace laya
 {
+#if defined(TRACY_PROFILER_ENABLED)
 static std::stack<TracyCZoneCtx> s_stackTracyZone;
+#endif
 void Profiler_ZoneStartForJS(const std::string &strName)
 {
 #if defined(TRACY_PROFILER_ENABLED)
