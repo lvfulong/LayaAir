1df3b206b045f153b900b13a7361b1c1efd79645
helloworldlv <775331175@qq.com>
2025-05-07 18:47:30 +0800

    字体解析缓存测试

--- conch/src/2D/benchmark/BenchmarkFontParser.cpp
+++ conch/src/2D/benchmark/BenchmarkFontParser.cpp
@@ -1,24 +1,46 @@
 #include <benchmark/benchmark.h>
 #include "../src/font_parser/FontParser.h"
 #include "../src/FontDescriptionParser.h"
+#include <utils/LRUCache.h>
 
 using namespace laya;
 
 static void BM_FontParserOld(benchmark::State& state) {
+    int i = 0;
     for (auto _ : state) {
         FontDescriptionParser parser;
         FontDescription out;
-        parser.parse("20px Arial", out);
+        auto font = std::to_string(i % 256) + "px Arial";
+        i++;
+        parser.parse(font, out);
     }
 }
 BENCHMARK(BM_FontParserOld)->Iterations(10000);
 
 static void BM_FontParserNew(benchmark::State& state) {
+    int i = 0;
     for (auto _ : state) {
         bool success;
-        FontProperties p = FontParser::parse("20px Arial", &success);
+        auto font = std::to_string(i % 256) + "px Arial";
+        i++;
+        FontProperties p = FontParser::parse(font, &success);
     }
 }
 BENCHMARK(BM_FontParserNew)->Iterations(10000);
 
+static LRUCache<std::string, FontProperties> cache(256);
+
+static void BM_FontParserNewWithCache(benchmark::State& state) {
+    int i = 0;
+    bool success;
+    for (auto _ : state) {
+        auto font = std::to_string(i % 256) + "px Arial";
+        i++;
+        auto p = cache.get(font);
+        if (!p) {
+            cache.put(font, FontParser::parse(font, &success));
+        }
+    }
+}
+BENCHMARK(BM_FontParserNewWithCache)->Iterations(10000);
 BENCHMARK_MAIN();
