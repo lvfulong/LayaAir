4d93481c3b008a7829e33964913014eb4bf39555
helloworldlv <775331175@qq.com>
2025-05-23 14:58:11 +0800

    fix

--- conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/BatchManager.cpp
+++ conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/BatchManager.cpp
@@ -1,32 +1,18 @@
 #include "BatchManager.h"
 #include <unordered_map>
-#include <vector>
 #include <utils/Log.h>
+#include <vector>
 
 namespace laya
 {
-// 静态成员初始化
-std::vector<Batch2DInfo*> Batch2DInfo::_pool;
-std::unordered_map<int, IBatch2DRender*> BatchManager::_batchMapManager;
+std::vector<Batch2DInfo *> Batch2DInfo::_pool;
+std::unordered_map<int, IBatch2DRender *> BatchManager::_batchMapManager;
 
-// Batch2DInfo实现
-Batch2DInfo* Batch2DInfo::create() {
-    if (!_pool.empty()) {
-        Batch2DInfo* info = _pool.back();
-        _pool.pop_back();
-        return info;
-    }
-    return new Batch2DInfo();
-}
-
-void Batch2DInfo::recover(Batch2DInfo* info) {
-    _pool.push_back(info);
-}
-
-// BatchManager实现
-void BatchManager::regisBatch(int renderElementType, IBatch2DRender* batch) {
-    if (_batchMapManager.find(renderElementType) != _batchMapManager.end()) {
-        LOGE("Overlapping batch optimization");
+void BatchManager::regisBatch(int renderElementType, IBatch2DRender *batch)
+{
+    if (_batchMapManager.find(renderElementType) != _batchMapManager.end())
+    {
+        LOGE("BatchManager::regisBatch Overlapping batch optimization");
     }
     _batchMapManager[renderElementType] = batch;
 }
--- conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/BatchManager.h
+++ conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/BatchManager.h
@@ -1,24 +1,21 @@
 #ifndef __BATCHMANAGER_H__
 #define __BATCHMANAGER_H__
 
+#include <unordered_map>
 #include <utils/FastSinglelist.h>
 #include <vector>
-#include <unordered_map>
 
 namespace laya
 {
-    class GLESRenderElement2D;
-// 合批渲染接口
+class GLESRenderElement2D;
 class IBatch2DRender
 {
   public:
     virtual ~IBatch2DRender() = default;
-    // 合批范围，合批的RenderElement2D直接add进list中
-    virtual void batchRenderElement(FastSinglelist<GLESRenderElement2D*> &list, int start, int length) = 0;
+    virtual void batchRenderElement(FastSinglelist<GLESRenderElement2D *> &list, int start, int length) = 0;
     virtual void recover() = 0;
 };
 
-// 合批信息类
 class Batch2DInfo
 {
   public:
@@ -28,14 +25,31 @@ class Batch2DInfo
     int elementLength = 0;
     int elementCount = 0;
 
-    static Batch2DInfo *create();
-    static void recover(Batch2DInfo *info);
+    Batch2DInfo()
+    {
+        _pool.reserve(256);
+    }
+
+    static Batch2DInfo *create()
+    {
+        if (!_pool.empty())
+        {
+            Batch2DInfo *info = _pool.back();
+            _pool.pop_back();
+            return info;
+        }
+        return new Batch2DInfo();
+    }
+
+    static void recover(Batch2DInfo *info)
+    {
+        _pool.push_back(info);
+    }
 
   private:
     static std::vector<Batch2DInfo *> _pool;
 };
 
-// 合批管理器
 class BatchManager
 {
   public:
@@ -43,6 +57,6 @@ class BatchManager
 
   private:
     static std::unordered_map<int, IBatch2DRender *> _batchMapManager;
-}; 
+};
 } // namespace laya
 #endif // __BATCHMANAGER_H__
