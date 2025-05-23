5c3539c153dc1e1cf4fc8b3a585a82a3be92ec54
helloworldlv <775331175@qq.com>
2025-05-23 12:03:41 +0800

    add

---
+++ conch/src/render/Const.h
@@ -0,0 +1,16 @@
+#ifndef __Const_H_
+#define __Const_H_
+namespace laya
+{
+enum class Const
+{
+    ENUM_TEXTALIGN_DEFAULT = 0,
+    ENUM_TEXTALIGN_CENTER = 1,
+    ENUM_TEXTALIGN_RIGHT = 2,
+
+    INDEX_BYTES = 2,
+
+    MAX_CLIP_SIZE = 99999999,
+};
+} // namespace laya
+#endif
