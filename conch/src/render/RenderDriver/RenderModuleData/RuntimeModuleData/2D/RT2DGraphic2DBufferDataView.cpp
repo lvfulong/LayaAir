0757068555d6d8a56a06b9ed450a39bc676556ba
helloworldlv <775331175@qq.com>
2025-05-26 17:22:58 +0800

    shit

--- conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/RT2DGraphic2DBufferDataView.cpp
+++ conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/RT2DGraphic2DBufferDataView.cpp
@@ -1,26 +1,33 @@
 #include "RT2DGraphic2DBufferDataView.h"
 #include "RTRender2DPass.h"
+#include "render/RenderDriver/OpenGLESDriver/RenderDevice/GLESIndexBuffer.h"
+#include "render/RenderDriver/OpenGLESDriver/RenderDevice/GLESVertexBuffer.h"
 #include <utils/Log.h>
-#include <jsbind/JSBind.h>
+#include <utils/Preprocessor.h>
+namespace laya
+{
 
-namespace laya {
-
-RT2DGraphicWholeBuffer::RT2DGraphicWholeBuffer() 
-    : _needResetData(false), _inPass(false) {
+RT2DGraphicWholeBuffer::RT2DGraphicWholeBuffer() : _needResetData(false), _inPass(false)
+{
     _updateRange.setValue(100000000, -100000000);
 }
 
-RT2DGraphicWholeBuffer::~RT2DGraphicWholeBuffer() {
-    destroy();
+RT2DGraphicWholeBuffer::~RT2DGraphicWholeBuffer()
+{
 }
 
-void RT2DGraphicWholeBuffer::resetData(int byteLength) {
+/*void RT2DGraphicWholeBuffer::resetData(int byteLength) {
     _needResetData = true;
     _updateRange.setValue(0, byteLength);
 
+    GET_ENV
+    jsvm_status status;
+    jsvm_value value;
     if (BufferModifyType::Index == modifyType) {
-        Uint16Array* newData = new Uint16Array(byteLength / 2);
-        if (bufferData.uint16Array) {
+        status = jsvm_create_typedarray(env, jsvm_uint16_array, byteLength / 2, wholeData, this->_start * 2, &value);
+        DEBUG_CHECK(status == jsvm_status::jsvm_ok);
+
+        if (_buffers.isValid()) {
             newData->set(bufferData.uint16Array);
         }
         bufferData.uint16Array = newData;
@@ -32,58 +39,68 @@ void RT2DGraphicWholeBuffer::resetData(int byteLength) {
         bufferData.floatArrays.clear();
         bufferData.floatArrays.push_back(newData);
     }
+}*/
+void RT2DGraphicWholeBuffer::setBuffers(jsvm_value value)
+{
+    // todo
 }
-
-void RT2DGraphicWholeBuffer::upload() {
-    if (_needResetData) {
-        if (BufferModifyType::Index == modifyType) {
-            buffers.indexBuffer->_setIndexData(bufferData.uint16Array, 0);
-        } else {
-            buffers.vertexBuffers[0]->setData(
-                bufferData.floatArrays[0]->buffer, 
-                0, 
-                0, 
-                bufferData.floatArrays[0]->byteLength
-            );
+void RT2DGraphicWholeBuffer::upload()
+{
+    if (_needResetData)
+    {
+        if (BufferModifyType::Index == _modifyType)
+        {
+            buffers.indexBuffer->_setIndexData((char *)_uint16ArrayBufferData, uint16ArrayByteLength, 0);
+        }
+        else
+        {
+            buffers.vertexBuffers[0]->setData((char *)floatArrays0, floatArrays0ByteLength, 0, 0,
+                                              floatArrays0ByteLength);
         }
 
-        for (auto view : _views) {
-            view->updateView(&bufferData);
+        for (auto view : _views)
+        {
+            view->updateView(this->_bufferData.getHandle());
         }
         _needResetData = false;
-    } else {
-        if (_updateRange.y <= _updateRange.x) return;
+    }
+    else
+    {
+        if (_updateRange.y <= _updateRange.x)
+            return;
 
-        if (BufferModifyType::Index != modifyType) {
-            buffers.vertexBuffers[0]->setData(
-                bufferData.floatArrays[0]->buffer,
-                _updateRange.x * 4,
-                _updateRange.x * 4,
-                (_updateRange.y - _updateRange.x) * 4
-            );
-        } else {
-            Uint16Array* tempView = new Uint16Array(
-                bufferData.uint16Array->buffer,
-                _updateRange.x * 2,
-                _updateRange.y - _updateRange.x
-            );
-            buffers.indexBuffer->_setIndexData(tempView, _updateRange.x * 2);
+        if (BufferModifyType::Index != _modifyType)
+        {
+            buffers.vertexBuffers[0]->setData((char *)floatArrays0, floatArrays0ByteLength, _updateRange.x * 4,
+                                              _updateRange.x * 4, (_updateRange.y - _updateRange.x) * 4);
+        }
+        else
+        {
+
+            int32_t offset = _updateRange.x * 2;
+            auto length = _updateRange.y - _updateRange.x;
+            uint16_t *tempView = _uint16ArrayBufferData + offset;
+            buffers.indexBuffer->_setIndexData((char *)tempView, length, _updateRange.x * 2);
             delete tempView;
         }
     }
     _updateRange.setValue(100000000, -100000000);
 }
 
-void RT2DGraphicWholeBuffer::modifyOneView(RT2DGraphic2DBufferDataView* view) {
-    _updateRange.y = std::max(view->start + view->length, _updateRange.y);
-    _updateRange.x = std::min(view->start, _updateRange.x);
+void RT2DGraphicWholeBuffer::modifyOneView(RT2DGraphic2DBufferDataView *view)
+{
+    _updateRange.y = std::max(double(view->_start + view->_length), _updateRange.y);
+    _updateRange.x = std::min(double(view->_start), _updateRange.x);
 }
 
-void RT2DGraphicWholeBuffer::addDataView(RT2DGraphic2DBufferDataView* view) {
+void RT2DGraphicWholeBuffer::addDataView(RT2DGraphic2DBufferDataView *view)
+{
     _views.push_back(view);
 }
 
-void RT2DGraphicWholeBuffer::destroy() {
+void RT2DGraphicWholeBuffer::destroy()
+{
+#if 0
     _views.clear();
     if (BufferModifyType::Index == modifyType) {
         delete bufferData.uint16Array;
@@ -94,69 +111,73 @@ void RT2DGraphicWholeBuffer::destroy() {
         }
         bufferData.floatArrays.clear();
     }
+#endif
 }
 
-RT2DGraphic2DBufferDataView::RT2DGraphic2DBufferDataView(
-    RT2DGraphicWholeBuffer* owner, 
-    BufferModifyType type,
-    int start, 
-    int length, 
-    int stride
-) : owner(owner), modifyType(type), _start(start), _length(length), 
-    stride(stride), isModified(false) {
-    updateView(&owner->bufferData);
+RT2DGraphic2DBufferDataView::RT2DGraphic2DBufferDataView(RT2DGraphicWholeBuffer *owner, BufferModifyType type,
+                                                         int start, int length, int stride)
+    : owner(owner), modifyType(type), _start(start), _length(length), stride(stride), isModified(false)
+{
+    updateView(owner->_bufferData.getHandle());
     owner->addDataView(this);
 }
 
-RT2DGraphic2DBufferDataView::~RT2DGraphic2DBufferDataView() {
+RT2DGraphic2DBufferDataView::~RT2DGraphic2DBufferDataView()
+{
 }
 
-jsvm_value RT2DGraphic2DBufferDataView::getData() {
-    if (owner->_needResetData) {
-        updateView(&owner->bufferData);
+jsvm_value RT2DGraphic2DBufferDataView::getData()
+{
+    if (owner->_needResetData)
+    {
+        updateView(owner->_bufferData.getHandle());
     }
     return _data.getHandle();
 }
 
-void RT2DGraphic2DBufferDataView::modify() {
+void RT2DGraphic2DBufferDataView::modify()
+{
     owner->modifyOneView(this);
-   // RTRender2DPass::setBuffer(owner); lvtodo
+    // RTRender2DPass::setBuffer(owner); lvtodo
 }
 
-void RT2DGraphic2DBufferDataView::updateView(jsvm_value wholeData) {
+void RT2DGraphic2DBufferDataView::updateView(jsvm_value wholeData)
+{
     if (modifyType == BufferModifyType::Index)
     {
         GET_ENV
         jsvm_status status;
         jsvm_value value;
-        status = jsvm_create_typedarray(env, jsvm_uint16_array, this->_length, wholeData, this->_start * 2/*Uint16Array.BYTES_PER_ELEMENT*/, &value);
+        status = jsvm_create_typedarray(env, jsvm_uint16_array, this->_length, wholeData,
+                                        this->_start * 2 /*Uint16Array.BYTES_PER_ELEMENT*/, &value);
         DEBUG_CHECK(status == jsvm_status::jsvm_ok);
         _data = jsbind::Persistent(value);
-        DEBUG_CHECK(status == jsvm_status::jsvm_ok);
-    } 
-    else 
+    }
+    else
     {
         GET_ENV
         jsvm_value array;
         jsvm_status status;
-
+        // this._data = [];
         status = jsvm_create_array_with_length(env, 0, &array);
         DEBUG_CHECK(status == jsvm_status::jsvm_ok);
         _data = jsbind::Persistent(array);
 
-        GET_ENV
         bool isArray;
-        jsvm_status status;
         status = jsvm_is_array(env, wholeData, &isArray);
         DEBUG_CHECK(status == jsvm_status::jsvm_ok);
         DEBUG_CHECK(isArray);
 
+        // let newData = (wholeData as Float32Array[])[0];
         jsvm_value newData;
         status = jsvm_get_element(env, wholeData, 0, &newData);
         DEBUG_CHECK(status == jsvm_status::jsvm_ok);
 
+        // this._data[0] = new (newData.constructor as any)(newData.buffer, this.start * newData.BYTES_PER_ELEMENT,
+        // this.length);
         jsvm_value float32_array;
-        status = jsvm_create_typedarray(env, jsvm_float32_array, this->_length, wholeData, this->_start * 4/*Float32Array.BYTES_PER_ELEMENT*/, &float32_array);
+        status = jsvm_create_typedarray(env, jsvm_float32_array, this->_length, wholeData,
+                                        this->_start * 4 /*Float32Array.BYTES_PER_ELEMENT*/, &float32_array);
         DEBUG_CHECK(status == jsvm_status::jsvm_ok);
 
         status = jsvm_set_element(env, array, 0, float32_array);
@@ -164,6 +185,4 @@ void RT2DGraphic2DBufferDataView::updateView(jsvm_value wholeData) {
     }
 }
 
-
-
-} // namespace laya 
+} // namespace laya
--- conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/RT2DGraphic2DBufferDataView.h
+++ conch/src/render/RenderDriver/RenderModuleData/RuntimeModuleData/2D/RT2DGraphic2DBufferDataView.h
@@ -1,77 +1,76 @@
-#ifndef __RT_2D_GRAPHIC_2D_BUFFER_DATA_VIEW_H__    
+#ifndef __RT_2D_GRAPHIC_2D_BUFFER_DATA_VIEW_H__
 #define __RT_2D_GRAPHIC_2D_BUFFER_DATA_VIEW_H__
 
-#include "../../../DriverDesign/RenderDevice/IIndexBuffer.h"
-#include "../../../DriverDesign/RenderDevice/IVertexBuffer.h"
-#include "../../Design/2D/IRender2DDataHandle.h"
-#include "../../../../maths/Vector2.h"
+#include <core/math/Vector2.h>
+#include <jsbind/JSBind.h>
 #include <vector>
+namespace laya
+{
 
-namespace laya {
-
-     enum class BufferModifyType {
-        Vertex = 0,
-        Index = 1,
-    };
-class RT2DGraphicWholeBuffer {
-public:
+enum class BufferModifyType
+{
+    Vertex = 0,
+    Index = 1,
+};
+class GLESIndexBuffer;
+class GLESVertexBuffer;
+class RT2DGraphic2DBufferDataView;
+class RT2DGraphicWholeBuffer
+{
+  public:
     union {
-        IIndexBuffer* indexBuffer;
-        std::vector<IVertexBuffer*> vertexBuffers;
+        GLESIndexBuffer *indexBuffer;
+        std::vector<GLESVertexBuffer *> vertexBuffers;
     } buffers;
-    
-    union {
-        std::vector<Float32Array*> floatArrays;
-        Uint16Array* uint16Array;
-    } bufferData;
-    
-    BufferModifyType modifyType;
+    void setBuffers(jsvm_value value);
+
+    uint16_t *_uint16ArrayBufferData = nullptr;
+    int32_t uint16ArrayByteLength = 0;
+
+    float *floatArrays0 = nullptr;
+    int32_t floatArrays0ByteLength = 0;
+
+    jsbind::Persistent _bufferData;
+    BufferModifyType _modifyType;
     bool _needResetData;
     bool _inPass;
 
     RT2DGraphicWholeBuffer();
     ~RT2DGraphicWholeBuffer();
 
-    void resetData(int byteLength) override;
-    void upload() override;
-    void modifyOneView(RT2DGraphic2DBufferDataView* view);
-    void addDataView(RT2DGraphic2DBufferDataView* view);
-    void destroy() override;
+    // void resetData(int byteLength);
+    void upload();
+    void modifyOneView(RT2DGraphic2DBufferDataView *view);
+    void addDataView(RT2DGraphic2DBufferDataView *view);
+    void destroy();
 
-private:
-    std::vector<RT2DGraphic2DBufferDataView*> _views;
+  private:
+    std::vector<RT2DGraphic2DBufferDataView *> _views;
     Vector2 _updateRange;
 };
 
 class RT2DGraphic2DBufferDataView
 {
-public:
-    RT2DGraphic2DBufferDataView(RT2DGraphicWholeBuffer* owner, BufferModifyType type, 
-                                int start, int length, int stride = 1);
+  public:
+    RT2DGraphic2DBufferDataView(RT2DGraphicWholeBuffer *owner, BufferModifyType type, int start, int length,
+                                int stride = 1);
     ~RT2DGraphic2DBufferDataView();
-
-    union {
-        std::vector<Float32Array*> floatArrays;
-        Uint16Array* uint16Array;
-    } _data;
-
-    int _start;      // element start
-    int _length;     // element length
-    int stride = 1;     // element stride
-    RT2DGraphicWholeBuffer* owner;
+    int _start;     // element start
+    int _length;    // element length
+    int stride = 1; // element stride
+    RT2DGraphicWholeBuffer *owner;
     BufferModifyType modifyType;
     bool isModified = false;
-    jsbind::Persistent _data;// Float32Array[] | Uint16Array;
+    jsbind::Persistent _data; // Float32Array[] | Uint16Array;
     jsvm_value getData();
     void modify();
     void updateView(jsvm_value wholeData);
-    std::pair<int, int> getDataRange() {
-        return { _start, _length };
+    std::pair<int, int> getDataRange()
+    {
+        return {_start, _length};
     }
-private:
-    void initData();
 };
 
 } // namespace laya
 
-#endif // __RT_2D_GRAPHIC_2D_BUFFER_DATA_VIEW_H__ 
+#endif // __RT_2D_GRAPHIC_2D_BUFFER_DATA_VIEW_H__
