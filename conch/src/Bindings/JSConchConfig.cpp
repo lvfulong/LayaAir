39c86ce8443af7b03ce40092430bfb00cade9a59
helloworldlv <775331175@qq.com>
2025-07-10 16:54:17 +0800

    版本号

--- conch/src/Bindings/JSConchConfig.cpp
+++ conch/src/Bindings/JSConchConfig.cpp
@@ -309,15 +309,15 @@ void JSConchConfig::setScreenOrientation(int p_nOrientation)
     const char* JSConchConfig::getRuntimeVersion()
     {
 #ifdef OS_IOS
-        return "ios-conch6-release-3.3.0-beta.4";
+        return "ios-conch6-release-3.3.0-2d-20250710";
 #elif OS_ANDROID
-        return "android-conch6-release-3.3.0-beta.4";
+        return "android-conch6-release-3.3.0-2d-20250710";
 #elif OS_WINDOWS
-        return "window-conch6-release-3.3.0-beta.4";
+        return "window-conch6-release-3.3.0-2d-20250710";
 #elif OS_OHOS
-        return "ohos-conch6-release-3.3.0-beta.4";
+        return "ohos-conch6-release-3.3.0-2d-20250710";
 #elif OS_LINUX
-        return "linux-conch6-release-3.3.0-beta.4";
+        return "linux-conch6-release-3.3.0-2d-20250710";
 #endif
     }
 	//机型
